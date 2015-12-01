e9308bfdb56c3b38bb94157963c14dca771ffb37
yyCaiGH <375189768@qq.com>
2015-12-01 18:02:15 +0800

    提交getSysImage类

---
+++ yUtils/src/org/cyy/yutils/sysfunction/GetSysImage.java
@@ -0,0 +1,28 @@
+package org.cyy.yutils.sysfunction;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+public class GetSysImage {
+
+	/**
+	 * return-data = true
+	 * 从相册中获取照片，并直接裁剪
+	 */
+	public static void fromAlbumAndCrop(Activity act,int requestCode){
+		Intent intent = new Intent(Intent.ACTION_PICK, null);
+		intent.setType("image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 200);
+		intent.putExtra("outputY", 200);
+		intent.putExtra("scale", true);
+		intent.putExtra("return-data", true);
+		// intent.putExtra("outputFormat",
+		// Bitmap.CompressFormat.JPEG.toString());
+		//intent.putExtra("noFaceDetection", false); // no face detection
+		act.startActivityForResult(intent, requestCode);
+	}
+}
